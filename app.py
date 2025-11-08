b68e08da36801e3407d162738179f443c8a36953
Alielden7 <nivis2077@gmail.com>
2025-11-08 14:18:54 +0200

    Update app.py

--- app.py
+++ app.py
@@ -35,3 +35,7 @@ def index():
         text_to_speech(text, lang)
         return send_file("output.mp3", as_attachment=True)
     return render_template("index.html")
+if __name__ == "__main__":
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
